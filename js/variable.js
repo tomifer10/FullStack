f04526dcda8bb9a0f643b3960764c04e677952b4
Tomas Fernandez <fernandeztomasig@gmail.com>
2022-05-17 13:57:27 +0200

    Primer dia Js

---
+++ js/variable.js
@@ -0,0 +1,19 @@
+// kebap-case (no permitido en JavaScript)
+//const last-name = 'Doe';
+// snake_case (recomendado en otros lenguajes como Python)
+const last_name = 'last_name';
+// PascalCase (recomendado en JavaScript para las clases)
+const LastName = 'LastName';
+// camelCase (recomendado en JavaScript para las variables y funciones)
+const lastName = 'lastName';
+document.write(LastName);
+const person = 'John Doe', carName = 'Volvo', price = 200;
+// equivalente a poner const con cada uno
+let x; //creando la variable
+x= 5; //asignando valor del tipo numero entero y valor 5
+x= "texto" // Asignando valor, de tipo String y valor Texto
+let variable4;
+// undefined
+console.log(variable4);
+// error
+console.log(variable_nodeclarada);
